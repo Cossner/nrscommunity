d9b33bdcdd0d28cdcd52fe76521cd51109c1b793
Daniel Pareja Gomez <Daniel Pareja Gomez@192.168.1.8>
2017-05-03 21:11:37 -0400

    stuff

---
+++ config/modules.config.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+	'Zend\Cache',
+	'Zend\Db',
+	'Zend\Log',
+	'Zend\Mail',
+	'Zend\Mvc\Console',
+	'Zend\Mvc\I18n',
+	'Zend\I18n',
+	'Zend\Mvc\Plugin\FilePrg',
+	'Zend\Mvc\Plugin\FlashMessenger',
+	'Zend\Mvc\Plugin\Identity',
+	'Zend\Mvc\Plugin\Prg',
+	'Zend\Navigation',
+	'Zend\Serializer',
+	'Zend\ServiceManager\Di',
+	'Zend\Paginator',
+	'Zend\Session',
+	'Zend\Form',
+	'Zend\InputFilter',
+	'Zend\Filter',
+	'Zend\Hydrator',
+	'Zend\Router',
+	'Zend\Validator',
+	'Application',
+	/*'Autoriza',
+	'DoctrineModule',
+	'DoctrineORMModule',
+	'AuthDoctrine',
+	/*
+    'Zend\Router',
+    'Zend\Validator',
+    'Application',*/
+];
